dce19f4b841e7aacf875097785888d91d434453e
storres20 <sanchotorres777@gmail.com>
2022-08-14 11:49:11 -0500

    fix(web): REMOVE commits

--- Front-End/web/src/http-common.js
+++ Front-End/web/src/http-common.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:3001/api", //localhost
-  //baseURL: "http://localhost:6868/api", //docker
-  //baseURL: "https://apicrudreal.herokuapp.com/api",
-  //baseURL: "https://real-back.vercel.app/api",
   baseURL: "https://pacientes20-back.herokuapp.com/api",
   
   headers: {
