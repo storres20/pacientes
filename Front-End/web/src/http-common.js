2db9bf5fa9bad34b0915694ae77341f8e6f319ed
storres20 <sanchotorres777@gmail.com>
2022-12-03 15:50:29 -0500

    Update backend baseURL

--- Front-End/web/src/http-common.js
+++ Front-End/web/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:3001/api", //localhost
-  baseURL: "https://pacientes20-back.herokuapp.com/api",
+  baseURL: "https://pacientes20-back.vercel.app/api",
   
   headers: {
     "Content-type": "application/json"
