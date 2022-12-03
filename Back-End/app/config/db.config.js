501fc6d23f9bb3267830c3519df7a21f0db6782d
storres20 <sanchotorres777@gmail.com>
2022-12-03 15:50:36 -0500

    Remove backend folder

--- Back-End/app/config/db.config.js
+++
@@ -1,7 +0,0 @@
-//clever cloud
-module.exports = {
-  HOST: "bct10i5bxqghdheg6nu1-mysql.services.clever-cloud.com",
-  USER: "uz7nzv8pvogs30u8",
-  PASSWORD: "6Tgus2xAhsO3bPYOxcJa",
-  DB: "bct10i5bxqghdheg6nu1"
-};
