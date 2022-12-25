f6962fa73a99cf5802edb89aa519c515e98a7d15
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-25 13:56:09 -0500

    Update http-common file

--- Front-End/web/src/http-common.js
+++ Front-End/web/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:3001/api", //localhost
-  baseURL: "https://pacientes20-back.vercel.app/api",
+  baseURL: "https://pacientesmongo-back.vercel.app/api",
   
   headers: {
     "Content-type": "application/json"
