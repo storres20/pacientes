37831f4f0e241e09002c4cc3b58da45b59f3088c
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-25 19:08:20 -0500

    Add footer component and styles

---
+++ Front-End/web/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <div className='footerContainer'>
+      <p>Designed by <a href='https://myport20.netlify.app/' target='_blank' rel="noreferrer"><b>ITALO</b></a></p>
+    </div>
+  )
+}
+
+export default Footer
